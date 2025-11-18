6ec95035d0b199f0a27a7616f702fd5b57d0a9b0
HimSha98 <himanshu.sharma.1013@gmail.com>
2025-11-18 22:08:53 +0530

    Igniting the App is complete.

--- App.js
+++ App.js
@@ -9,14 +9,14 @@ const parent = React.createElement('div',{id: "parent"},
     [
         React.createElement('div', {id: "child"}, 
             [
-                React.createElement('h1', {key: "h1a"}, "I am h1 tag!"),
+                React.createElement('h1', {key: "h1a"}, "I am Himanshu Sharma."),
                 React.createElement('h2', {key: "h2a"}, "I am h2 tag!")
             ]
         ),
         React.createElement('div', {id: "child"}, 
             [
                 React.createElement('h1', {key: "h1b"}, "I am h1 tag!"),
-                React.createElement('h2', {key: "h1b"}, "I am h2 tag!")
+                React.createElement('h2', {key: "h2b"}, "I am h2 tag!")
             ]
         )
     ]
--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # STARTING REACT BY HIMANSHU
+
+#PARCEL
+- Dev Build
+- Local Server
+- HMR = Hot Module Replacement (Automatically Refreshing the page when changes made.)
+- It uses File Watching Algorithm - written in C++ to do HMR
+- Gives Faster Builds - By Caching
+- Image Optimization
+- Minification of Files in production build
+- Bundling
+- Compress
+- Consistent Hashing
+- Code Splitting
+- Differentiate Bundling
+- Diagnostic
+- Error Handling
+- Tree Shaking - remove unused code
+- Different dev and production bundles
